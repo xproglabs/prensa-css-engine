fdcaeefb13d69c045698d2825b9563451d66cc22
gbombassaro <giovanni.bombassaro@gmail.com>
2022-02-14 22:03:34 -0300

    feat: Otimiza função boxShadow

---
+++ cssEngine/boxShadow/boxShadow.test.ts
@@ -0,0 +1,44 @@
+import { boxShadow } from './index'
+
+describe('boxShadow styled function', () => {
+
+  const theme = {
+    queries: {
+      xs: '360px',
+      sm: '460px',
+      md: '768px',
+      lg: '1016px',
+      xl: '1280px'
+    }
+  }
+
+  test('empty', () => {
+    expect(
+      boxShadow({})
+    ).toBe(
+      ''
+    )
+  })
+
+  test('boxShadow', () => {
+    expect(
+      boxShadow({
+        boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.1)',
+        theme
+      })
+    ).toBe(
+      'box-shadow:5px5px10pxrgba(0,0,0,0.1);'
+    )
+  })
+
+  test('boxShadow responsive', () => {
+    expect(
+      boxShadow({
+        boxShadow: ['2.5px 2.5px 5px rgba(0, 0, 0, 0.1)', '5px 5px 10px rgba(0, 0, 0, 0.1)'],
+        theme
+      })
+    ).toBe(
+      'box-shadow:2.5px2.5px5pxrgba(0,0,0,0.1);@media(min-width:1016px){box-shadow:5px5px10pxrgba(0,0,0,0.1);}'
+    )
+  })
+})
--- cssEngine/boxShadow/index.ts
+++ cssEngine/boxShadow/index.ts
@@ -1,10 +1,31 @@
+import { BoxShadowProps } from '@xprog/prensa-css-engine/props'
+
+import { generateBoxShadow } from './parsers'
+import { boxShadowResponsive } from './responsive'
+
 /**
  * Prensa Styled System | boxShadow
  * @param props Expect a string matching box-shadow css requirements
  * boxShadow: string;
  * @returns a string with CSS syntax
  */
-export function boxShadow({ boxShadow }) {
-  if (!boxShadow) return ''
-  return `box-shadow: ${boxShadow};`
+export function boxShadow(props: BoxShadowProps) {
+
+  if (!props) return ''
+
+  const { boxShadow, theme } = props
+  const styles = []
+
+  styles.push(
+    typeof boxShadow === 'string' ? generateBoxShadow(boxShadow) : '',
+  )
+
+  styles.push(
+    boxShadowResponsive(
+      boxShadow,
+      theme
+    )
+  )
+
+  return styles.join('').replace(/\s+/g, '')
 }
