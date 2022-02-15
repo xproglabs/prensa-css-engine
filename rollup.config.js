06da00b6fb4f3dbe60874085b6fa91657559042e
gbombassaro <giovanni.bombassaro@gmail.com>
2022-02-14 21:34:59 -0300

    feat: Adiciona nova bundle para a função estilizada maxAndMin

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ export default [
   createBundle('cssEngine/color/index.ts', 'dist/color/index', 'color'),
   createBundle('cssEngine/height/index.ts', 'dist/height/index', 'height'),
   createBundle('cssEngine/margin/index.ts', 'dist/margin/index', 'margin'),
+  createBundle('cssEngine/maxAndMin/index.ts', 'dist/maxAndMin/index', 'maxAndMin'),
   createBundle('cssEngine/padding/index.ts', 'dist/padding/index', 'padding'),
   createBundle('cssEngine/radius/index.ts', 'dist/radius/index', 'radius'),
   createBundle('cssEngine/width/index.ts', 'dist/width/index', 'width')
