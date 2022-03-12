2a5b7c266e3ba5c2a58999997e847db34ddd44d6
gbombassaro <giovanni.bombassaro@gmail.com>
2022-03-12 03:16:19 -0300

    feat: Permite estilo responsivo na função customCSS

--- cssEngine/customCSS/index.ts
+++ cssEngine/customCSS/index.ts
@@ -1,6 +1,26 @@
 import { CustomCSSProps } from '@xprog/prensa-css-engine/props'
+import isArray from 'lodash/isArray'
 
-export function customCSS({ custom }: CustomCSSProps) {
+import { createResponsiveStyle } from '../responsiveEngine'
+
+export function customCSS({ custom, theme }: CustomCSSProps) {
   if (!custom) return ''
-  return custom.replace(/\s+/g, '')
+
+  const css = []
+
+  if (isArray(custom)) {
+    css.push(
+      createResponsiveStyle(
+        custom,
+        pos => custom[pos],
+        theme
+      )
+    )
+  } else {
+    css.push(
+      custom
+    )
+  }
+
+  return css.join('').replace(/\s+/g, '')
 }
