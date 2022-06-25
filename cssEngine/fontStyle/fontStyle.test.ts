88cf8375f4b0930c9f1bfd98c0964a30b8f6743f
gbombassaro <giovanni.bombassaro@gmail.com>
2022-06-25 12:39:18 -0300

    feat: Nova função fontStyle

---
+++ cssEngine/fontStyle/fontStyle.test.ts
@@ -0,0 +1,45 @@
+import { fontStyle } from './index'
+
+const theme = {
+  queries: {
+    xs: '360px',
+    sm: '460px',
+    md: '768px',
+    lg: '1016px',
+    xl: '1280px'
+  }
+}
+
+describe('fontStyle styled function', () => {
+  test('fontStyle', () => {
+    expect(
+      fontStyle({
+        fontStyle: 'italic',
+        theme
+      })
+    ).toBe(
+      'font-style:italic;'
+    )
+  })
+
+  test('fontStyle responsive (mobile/desktop)', () => {
+    expect(
+      fontStyle({
+        fontStyle: ['oblique', 'italic'],
+        theme
+      })
+    ).toBe(
+      'font-style:oblique;@media(min-width:460px){font-style:oblique;}@media(min-width:1016px){font-style:italic;}'
+    )
+  })
+  test('fontStyle responsive (mobile/tablet/desktop) ', () => {
+    expect(
+      fontStyle({
+        fontStyle: ['oblique', 'italic', 'normal'],
+        theme
+      })
+    ).toBe(
+      'font-style:oblique;@media(min-width:460px){font-style:oblique;}@media(min-width:768px){font-style:italic;}@media(min-width:1016px){font-style:normal;}'
+    )
+  })
+})
---
+++ cssEngine/fontStyle/types.ts
@@ -0,0 +1,3 @@
+import type * as CSS from 'csstype'
+
+export type FontStyleTypes = CSS.Properties['fontStyle'] | Array<CSS.Properties['fontStyle']>;
